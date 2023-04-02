47ba477e5368e8e0fdf435080b896a3f77836b28
Ziv Ben-Zuk <zivbenzuk@gmail.com>
2023-04-02 21:27:02 +0700

    integration simulation with animation

--- train_doppler_ui.js
+++ train_doppler_ui.js
@@ -18,9 +18,13 @@ $("#french_train_id").on("click", function () {
 $("#start_simulation_id").on("click", function () {
   playAudioFileFromURL(selected_train_audio_file);
   startSimulation();
+  // start the CSS annimation of the train
+  $("#train_div_id").addClass("train_animation");
 });
 
 $("#stop_simulation_id").on("click", function () {
   stopPlayback();
   stopSimulation();
+  // stop the CSS annimation of the train
+  $("#train_div_id").removeClass("train_animation");
 });
